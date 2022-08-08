8d12de909fb92f317be10764ee3f4d11c69a6954
saksham122 <sakshamtiwari.212@gmail.com>
2022-08-08 21:37:28 +0530

    Changes Done

---
+++ src/main/java/com/Product.java
@@ -0,0 +1,27 @@
+package com;
+
+public class Product
+{
+	private int Pid;
+	private String Pname;
+	private int Price;
+	public int getPid() {
+		return Pid;
+	}
+	public void setPid(int Pid) {
+		this.Pid = Pid;
+	}
+	public String getPname() {
+		return Pname;
+	}
+	public void setPname(String Pname) {
+		this.Pname = Pname;
+	}
+	public int getPrice() {
+		return Price;
+	}
+	public void setPrice(int Price) {
+		this.Price = Price;
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<a href="Index.html"> Product form</a>
+</body>
+</html>
